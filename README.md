7cdce6d922ea4c67d7c5237104182f09e71ef13a
Kancy Joe <54024877+sunflower2333@users.noreply.github.com>
2025-11-12 20:25:34 +0000

    add pineapple abl patch and clang modules.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Build ABL image in workflow
+
+## Build
+Run the following command to build abl for specific target
+```
+./build.sh <abl-dir> <patch-dir> <codename>
+```
--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 # Usage:
-#   ./build.sh <codename>
+#   ./build.sh <dir> <codename>
 # Example:
-#   ./build.sh pineapple
+#   ./build.sh abl-pineapple pineapple
 
 # SDLLVM Download:
 #   https://github.com/SuleMareVientu/Qualcomm_Snapdragon_LLVM_ARM_Toolchain_OEM/releases/download/16.1.3.0/16.1.3.0.zip
-TARGET=$1
+ABL_SRC=$1
+TARGET=$2
+
 if [ -z "$TARGET" ]; then
-    echo "Usage: $0 <codename>"
-    echo "Example: $0 pineapple"
+    echo "Usage:"
+    echo -e "\t$0  <dir> <codename>"
+    echo "Example:"
+    echo -e "\t$0 abl-pineapple pineapple"
     exit 1
 fi
 
 # Setting up env
 ROOT_DIR=$PWD/
-ABL_SRC=abl/
 source $ROOT_DIR/$ABL_SRC/QcomModulePkg/build.config.msm.$TARGET
 SDLLVM_PATH=$ROOT_DIR/sdllvm/
-SDLLVM_VERSION="16.1.3.0"
+SDLLVM_VERSION="10.0"
 
 if [ ! -d "$SDLLVM_PATH" ]; then
     echo "SDLLVM directory not found!"
@@ -30,7 +33,7 @@ fi
 
 # Build
 cd $ABL_SRC
-make all BOOTLOADER_OUT=out/ CLANG_BIN=/usr/bin/ "${MAKE_FLAGS[@]}"
+make all BOOTLOADER_OUT=out/ CLANG_BIN=$SDLLVM_PATH/$SDLLVM_VERSION/bin/ "${MAKE_FLAGS[@]}"
 cd ..
 
 # Sign
